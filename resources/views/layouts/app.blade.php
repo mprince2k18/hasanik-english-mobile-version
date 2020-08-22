cfa2e2513161af427e6c258964e18e14234625ed
mprince2k18 <mprince2k16@gmail.com>
2020-08-23 00:22:51 +0600

    nav update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-    <title>AppKit Mobile</title>
+    <title>Hasanik English</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend/styles/bootstrap.css') }}">
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
