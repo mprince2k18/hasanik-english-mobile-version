24515116052db50e50d37d8a5d060055ef8f9e43
mprince2k18 <mprince2k16@gmail.com>
2020-08-23 00:23:26 +0600

    nav update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,6 +100,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
     <title>Hasanik English</title>
